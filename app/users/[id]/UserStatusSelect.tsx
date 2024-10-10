50b4f5add67721a3c9bd5618ac067d6255dba478
Ziyad Hasen <hziyad933@gmail.com>
2024-10-10 14:42:45 +0300

    test

--- app/users/[id]/UserStatusSelect.tsx
+++ app/users/[id]/UserStatusSelect.tsx
@@ -1,8 +1,19 @@
 import { Status } from '@prisma/client'
+import { Select } from '@radix-ui/themes'
 import React from 'react'
 
 const UserStatusSelect = ({ userStatuses }: { userStatuses: Status }) => {
-  return <div>UserStatusSelect</div>
+  return (
+    <>
+      <Select.Root size='2' defaultValue='apple'>
+        <Select.Trigger />
+        <Select.Content>
+          <Select.Item value='apple'>Apple</Select.Item>
+          <Select.Item value='orange'>Orange</Select.Item>
+        </Select.Content>
+      </Select.Root>
+    </>
+  )
 }
 
 export default UserStatusSelect
